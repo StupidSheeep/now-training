2b03b0e9c92aea0584f15c5e707d75aa47a65cfc
KyoheiUtsunomiya <134346889+StupidSheeep@users.noreply.github.com>
2023-08-12 15:34:35 +0000

    [Add] model,controller

---
+++ db/migrate/20230812095152_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+
+      t.integer :user_id, null: false
+      t.integer :genre_id,  null: false
+      t.string  :title, null: false
+      t.text  :body,  null: false
+      t.integer :target_time, null: false
+
+      t.timestamps
+    end
+  end
+end
