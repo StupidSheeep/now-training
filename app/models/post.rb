2f49c1d070843d932a76ff6aaf12609bceca5b46
KyoheiUtsunomiya <134346889+StupidSheeep@users.noreply.github.com>
2023-08-14 06:44:04 +0000

    [Add] Admins Views

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+
+  has_one_attached :post_image
+
+  def get_post_image(width, height)
+    unless item_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      item_image.attach(io: File.open(file_path), filename: 'no-image.jpg', content_type: 'image/jpeg')
+    end
+    item_image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :profile_image
+
+  def get_profile_image(width, height)
+    unless item_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      item_image.attach(io: File.open(file_path), filename: 'no-image.jpg', content_type: 'image/jpeg')
+    end
+    item_image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
