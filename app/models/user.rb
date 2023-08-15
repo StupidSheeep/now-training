4a4ead2f5eb8870a7c13b2ac5d151eb838ec7c0b
KyoheiUtsunomiya <134346889+StupidSheeep@users.noreply.github.com>
2023-08-15 05:37:29 +0000

    [Add] association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # 他のユーザーをフォローしている関係
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id"
+  has_many :following, through: :active_relationships, source: :followed
+
+  # 自分をフォローしているユーザーの関係
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id"
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  has_many :bookmarks, dependent: :destroy
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
 end
