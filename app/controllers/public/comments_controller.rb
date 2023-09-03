68f5a5c96ede15a048bea50920d702edd642f70c
KyoheiUtsunomiya <134346889+StupidSheeep@users.noreply.github.com>
2023-09-03 04:42:23 +0000

    [update]Natural Language API

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -4,6 +4,7 @@ class Public::CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @post_comment = @post.comments.new(post_comment_params)
     @post_comment.user = current_user
+    @post_comment.score = Language.get_data(post_comment_params[:post_comment])  #この行を追加
     @post_comment.save
     redirect_to post_path(@post)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_14_053127) do
+ActiveRecord::Schema.define(version: 2023_09_03_043518) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2023_08_14_053127) do
     t.text "post_comment", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "score", precision: 5, scale: 3
   end
 
   create_table "genres", force: :cascade do |t|
